75d7149c9339691c7b6190c58cc71096d87dc1fb
david collett <davida.collett95@gmail.com>
2019-07-05 16:07:57 -0600

    more done

--- classes.js
+++ classes.js
@@ -64,10 +64,6 @@ class Employee {
 class Manager extends Employee {
   constructor(first_name, last_name, email, age) {
     super(first_name, last_name, email, age)
-    // this.first_name = first_name
-    // this.last_name = last_name
-    // this.email = email
-    // this.age = age
     this.reports = []
 
   }
